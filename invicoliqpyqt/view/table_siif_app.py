cb7c674cd77a41752609b69fe4e374f156a7b5d8
Fernando Corrales <fscorrales@gmail.com>
2022-07-10 23:19:54 -0300

    feat: add qTableWidget retenciones

--- invicoliqpyqt/view/table_siif_app.py
+++ invicoliqpyqt/view/table_siif_app.py
@@ -1,10 +1,12 @@
 import sys
 
-from invicoliqpyqt.view.table_siif import Ui_table_siif
 from invicoliqpyqt.model.models import ModelImputacionesSIIF
 from invicoliqpyqt.utils.delegates import FloatDelegate
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtCore import pyqtSlot, QItemSelection, QSortFilterProxyModel
+from invicoliqpyqt.view.table_siif import Ui_table_siif
+from PyQt5.QtCore import QItemSelection, QSortFilterProxyModel, pyqtSlot
+from PyQt5.QtSql import QSqlQuery
+from PyQt5.QtWidgets import QApplication, QTableWidgetItem, QWidget
+
 
 # Inherit from QMainWindow
 class TableSIIF(QWidget):
@@ -21,7 +23,6 @@ class TableSIIF(QWidget):
         self.proxy_imputaciones_siif = QSortFilterProxyModel(self)
         self.proxy_imputaciones_siif.setSourceModel(self.model_imputaciones_siif)
 
-
         #Connect view with model
         self.ui.table_comprobantes.setModel(self.model_comprobantes_siif)
         self.ui.table_imputaciones.setModel(self.proxy_imputaciones_siif)
@@ -32,19 +33,39 @@ class TableSIIF(QWidget):
         self.ui.table_comprobantes.setItemDelegateForColumn(3, FloatDelegate())
 
         #Set slot connection
-        self.ui.table_comprobantes.selectionModel().selectionChanged.connect(self.show_imputaciones)
+        self.ui.table_comprobantes.selectionModel().selectionChanged.connect(self.show_detail)
 
     @pyqtSlot(QItemSelection, QItemSelection)
-    def show_imputaciones(self, selected, deselected):
-        print("selected: ")
+    def show_detail(self, selected, deselected):
         for ix in selected.indexes():
+            #Get N° Entrada SIIF
             row = ix.row()
             cyo_id = self.model_comprobantes_siif.index(row, 0)
             cyo_id = self.model_comprobantes_siif.data(cyo_id, role=0)
+            #Update table imputaciones
             self.proxy_imputaciones_siif.layoutAboutToBeChanged.emit()
             self.proxy_imputaciones_siif.setFilterKeyColumn(0)
             self.proxy_imputaciones_siif.setFilterFixedString(str(cyo_id))
             self.proxy_imputaciones_siif.layoutChanged.emit()
+            #Update table retenciones
+            self.ui.table_retenciones.setRowCount(0)
+            self.ui.table_retenciones.setColumnCount(3)
+            self.ui.table_retenciones.setHorizontalHeaderLabels(["Estructura", "Partida", "Ejecutado"])
+            query = QSqlQuery('SELECT estructura, partida, ' + 
+                                'sum(importe_bruto) as ejecutado ' + 
+                                'FROM honorarios_factureros ' + 
+                                f'WHERE nro_entrada = "{cyo_id}" '
+                                'GROUP BY estructura, partida')
+            while query.next():
+                rows = self.ui.table_retenciones.rowCount()
+                self.ui.table_retenciones.setRowCount(rows + 1)
+                self.ui.table_retenciones.setItem(rows, 0, QTableWidgetItem(query.value(0)))
+                self.ui.table_retenciones.setItem(rows, 1, QTableWidgetItem(query.value(1)))
+                self.ui.table_retenciones.setItem(rows, 2, QTableWidgetItem(str(query.value(2))))
+            self.ui.table_retenciones.setItemDelegateForColumn(2, FloatDelegate())
+            self.ui.table_retenciones.resizeColumnsToContents()
+
+
 
         # print("deselected: ")
         # for ix in deselected.indexes():
